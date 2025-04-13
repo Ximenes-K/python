d1d19d0db91bf085710accbc75d7b5cab0ec915e
ximenes-k <ximenes.kelven@gmail.com>
2025-04-13 13:11:19 -0300

    if, elif, else

---
+++ Códigos iniciais.py
@@ -0,0 +1,19 @@
+n1=int(input('Informe a nota da N1: '))
+n2=int(input('informe a nota da N2: '))
+peso_1=float(input('informe o peso da N1: '))
+peso_2=float(input('informe o peso da N2: '))
+# só interessa a parte inteira da média.
+media=(n1*peso_1 + n2*peso_2)//(peso_1 + peso_2)
+aprovados= media < 7
+print('a sua média final é:',media)
+if media > 7:
+    print('você está aprovado. Boas festas!')
+elif media == 7:
+    print('você está de exame')
+elif 5 < media < 7:
+    print('Faça um trabalho para ajudar na nota' )
+else:
+    print('você está reprovado sem recuperação')
+
+
+
